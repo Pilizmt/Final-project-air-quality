2c1a30a7a1a5d00e4f49951de96456aa8534c121
Pilar Zarco <142118783+Pilizmt@users.noreply.github.com>
2024-02-03 19:15:36 +0000

    requirements

--- notebooks/API_Predict.py
+++ notebooks/API_Predict.py
@@ -10,3 +10,20 @@ dicc_municipios = load(open('../data/interim/diccionarios/dicc_municipios.pk', '
 dicc_provincias = load(open('../data/interim/diccionarios/dicc_provincias.pk', 'rb'))
 
 
+name_municipality = st.selectbox('Selecciona el nombre del Municipio:',
+                                 )
+
+
+
+# ipyleaflet>=0.14.0
+# ipywidgets>=7.7.1
+# matplotlib>=3.7.0
+# numpy>=1.24.2
+# opencv-python>=4.1.2
+# pandas>=1.5.3
+# python-dotenv>=0.20.0
+# requests>=2.27.1
+# scikit-learn>=1.3.2
+# seaborn>=0.12.2
+# streamlit==1.31.0
+# joblib
